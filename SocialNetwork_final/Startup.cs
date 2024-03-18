579e8f7fd711a4740f2f9007b0ef7b53cfea3868
Name <email@exaple.ru>
2024-03-18 11:51:24 +0300

    Update Startup

--- SocialNetwork_final/Startup.cs
+++ SocialNetwork_final/Startup.cs
@@ -1,6 +1,33 @@
-﻿namespace SocialNetwork_final
+﻿using Microsoft.AspNetCore.Builder;
+using SocialNetwork_final.Controllers;
+
+namespace SocialNetwork_final
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoint =>
+            {
+                endpoint.MapControllerRoute(
+                 name: "default",
+                 pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
     }
 }
